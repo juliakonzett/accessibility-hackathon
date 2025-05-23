b82a94183b26d3933504dc9065e70999cea60f93
Julia <julia.konzett@digit-one.com>
2025-05-23 17:22:54 +0200

    added sub menu items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
+const productItems = ['Produkte', 'Erfahrungen', 'FAQ'];
+const serviceItems = ['Leistungen', 'News'];
+const newsItems = ['News', 'Blog'];
+const contactItems = ['Über uns', 'Kontakt'];
+
 export default function Navbar() {
   return (
     <nav className='navbar px-[20%] py-[2%] hyphens-auto flex flex-row gap-4 justify-between items-center bg-blue-950 text-white'>
@@ -23,28 +28,41 @@ export default function Navbar() {
         tabIndex={0}
         href='#navigation'
         className='custom-focus'>
-        Reduzierte Navigation
+        Produkte
       </Link>
+      {productItems.length > 0 &&
+        productItems.map((item, index) => {
+          return (
+            <Link
+              key={index}
+              aria-label={`Scrollt zum Abschnitt der Seite des ${item} Abschnitts`}
+              tabIndex={0}
+              href={`#${item.toLowerCase()}`}
+              className='custom-focus'>
+              {item}
+            </Link>
+          );
+        })}
       <Link
         aria-label={'Scrollt zum Abschnitt der Seite der Skalierungsfunktion'}
         tabIndex={0}
         href='#scaling'
         className='custom-focus'>
-        Skalierungsfunktion
+        Leistungen
       </Link>
       <Link
         aria-label={'Scrollt zum Abschnitt der Seite der Hochkontrastfunktion'}
         tabIndex={0}
         href='#contrast'
         className='custom-focus'>
-        Hochkontrast
+        News
       </Link>
       <Link
         aria-label={'Scrollt zum Abschnitt der Seite des Back to Top Buttons'}
         tabIndex={0}
         href='#backToTop'
         className='custom-focus'>
-        Back to Top
+        Kontakt
       </Link>
     </nav>
   );
